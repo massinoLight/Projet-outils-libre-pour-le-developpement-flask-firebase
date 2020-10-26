0da8a6271d546eb1fdfcec4e906168193722134c
massino <nacerddine.amassin@gmail.com>
2020-10-26 14:34:19 +0100

    Commit la connection a firebase marche bien la page de login marche bien TODO ajouter le module flask

---
+++ firebaseUtils.py
@@ -0,0 +1,41 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import os
+###################################################
+#                                                  #
+# Des fonction pour accéder au cloud firbase       #
+# et deux fonctions l'une pour ajouter des données #
+# l'autre pour récupérer toute une colléction      #
+#                                                  #
+####################################################
+
+cred = credentials.Certificate("./serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+#cette ligne est tres importante pour importer les clés google comme environement de variables
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/massino/Bureau/projetOutilsLibres-658591671fcb.json"
+
+
+def add_data():
+    db = firestore.Client()
+    doc_ref = db.collection(u'Utilisateurs').document(u'Massino')
+    doc_ref.set({
+        u'first': u'Massino',
+        u'last': u'LightNight',
+        u'born': 1997,
+        u'email': u'massino@gmail.com'
+    })
+
+def get_collection():
+        db = firestore.Client()
+
+        users_ref = db.collection('Utilisateurs')
+        docs = users_ref.stream()
+
+        for doc in docs:
+            print(f'{doc.id} => {doc.to_dict()}')
+
+
+if __name__ == '__main__':
+    print("test firebase")
+    get_collection()
